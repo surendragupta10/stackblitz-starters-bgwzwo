36f196fecd4ddeb4ca711be7abc710929b4da9ca
surendragupta10 <surendragupta10@outlook.com>
2024-01-12 20:58:38 +0530

    subtodo

--- models/todos/todo.model.js
+++ models/todos/todo.model.js
@@ -1,5 +1,26 @@
 import mongoose from 'mongoose';
 
-const todoSchema = new mongoose.Schema({}, { timestamps: true });
+const todoSchema = new mongoose.Schema({
+    content : {
+        type: String,
+        required : true,
+
+    },
+    complete : {
+        type: Boolean,
+        default: false,
+            },
+            createdBy: {
+                type : mongoose.Schema.Types.ObjectId,
+                ref : "User"
+            },
+            subTodos: [
+                {
+                    type : mongoose.Schema.Types.ObjectId,
+                    ref : "SubTodo"
+                }
+            ] // Arrays of SubToDo
+    },
+{ timestamps: true });
 
 export const Todo = mongoose.model('Todo', todoSchema);
