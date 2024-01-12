6691b02e6745b20d110479d2894597f9d4f52241
surendragupta10 <surendragupta10@outlook.com>
2024-01-12 20:48:15 +0530

    todo

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,25 +1,5 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: [true, 'password is required'],
-    },
-  },
-  { timestamps: true }
-);
+const subTodoSchema = new mongoose.Schema({}, {timestamps : true})
 
-export const User = mongoose.model('User', userSchema);
+export const subTodo = mongoose.model("SubTodo", subTodoSchema)
